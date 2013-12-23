e110762ee37c5686345c1c28a971003dd5604d0b
aj0strow <alexander.ostrow@gmail.com>
2013-12-23 00:28:19 -0800

    af

--- spec/i18n/alphabets_spec.rb
+++ spec/i18n/alphabets_spec.rb
@@ -34,4 +34,13 @@ describe I18n::Alphabets do
     }
   end
   
+  describe '#af' do
+    subject { alphabets.af }
+    include_examples 'alphabet', %w{
+      A a	B b	C c	D d	E e	F f	G g	H h	I i
+      J j	K k	L l	M m	N n	O o	P p	Q q	R r
+      S s	T t	U u	V v	W w	X x	Y y	Z z	 
+    }
+  end
+  
 end
