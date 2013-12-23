bf3892ebd45e80b32e54ac62a027b6eb3b0b7490
aj0strow <alexander.ostrow@gmail.com>
2013-12-23 01:17:21 -0800

    ar

--- spec/i18n/alphabets_spec.rb
+++ spec/i18n/alphabets_spec.rb
@@ -90,4 +90,13 @@ describe I18n::Alphabets do
       \u1377 \u1378 \u1379 \u137A \u137B \u137C \u137D \u137E \u137F
     }
   end
+  
+  describe '#ar' do
+    subject { alphabets.ar }
+    include_examples 'alphabet', %W{
+      \u0627 \u0628 \u062A \u062B \u062C \u062D \u062E \u062F \u0630 \u0631 
+      \u0632 \u0633 \u0634 \u0635 \u0636 \u0637 \u0638 \u0639 \u063A \u0641 
+      \u0642 \u0643 \u0644 \u0645 \u0646 \u0647 \u0648 \u064A
+    }
+  end
 end
