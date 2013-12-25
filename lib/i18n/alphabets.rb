47476f6497f7a427e226c345c2705e236d99fd9b
aj0strow <alexander.ostrow@gmail.com>
2013-12-24 21:25:33 -0800

    ca

--- lib/i18n/alphabets.rb
+++ lib/i18n/alphabets.rb
@@ -343,5 +343,19 @@ module I18n
       }.to_set
     end
     
+    def ca
+      # http://en.wikipedia.org/wiki/Catalan_alphabet
+      %W{
+        \u0041 \u0042 \u0043 \u0044 \u0045 \u0046 \u0047 \u0048 \u0049 \u004A 
+        \u004B \u004C \u004D \u004E \u004F \u0050 \u0051 \u0052 \u0053 \u0054 
+        \u0055 \u0056 \u0057 \u0058 \u0059 \u005A \u0061 \u0062 \u0063 \u0064 
+        \u0065 \u0066 \u0067 \u0068 \u0069 \u006A \u006B \u006C \u006D \u006E 
+        \u006F \u0070 \u0071 \u0072 \u0073 \u0074 \u0075 \u0076 \u0077 \u0078 
+        \u0079 \u007A \u00C0 \u00C7 \u00C8 \u00C9 \u00CD \u00CF \u00D2 \u00D3 
+        \u00DA \u00DC \u00E0 \u00E7 \u00E8 \u00E9 \u00ED \u00EF \u00F2 \u00F3 
+        \u00FA \u00FC
+      }.to_set
+    end
+    
   end
 end
--- spec/i18n/alphabets_spec.rb
+++ spec/i18n/alphabets_spec.rb
@@ -280,4 +280,13 @@ describe I18n::Alphabets do
       \uAA76 \uAA77 \uAA78 \uAA79 \uAA7A \uAA7B
     }
   end
+  
+  describe '#ca' do
+    subject { alphabets.ca }
+    include_examples :alphabet, %W{
+      A	B	C	D	E	F	G	H	I	J	K	L	M	N	O	P	Q	R	S	T	U	V	W	X	Y	Z
+      a	b	c	d	e	f	g	h	i	j	k	l	m	n	o	p	q	r	s	t	u	v	w	x	y	z
+      À à É é È è Í í Ï ï Ó ó Ò ò Ú ú Ü ü Ç ç
+    }
+  end
 end
